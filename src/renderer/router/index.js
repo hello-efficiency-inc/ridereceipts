b535242205bc89910c502aba59701fb3477557eb
Meet Godhani <meetgodhani23@gmail.com>
2018-04-08 16:35:14 -0400

    Initial commit for Lyft integration

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,6 +6,16 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/uber',
+      name: 'uber',
+      component: require('@/components/Uber').default
+    },
+    {
+      path: '/lyft',
+      name: 'lyft',
+      component: require('@/components/Lyft').default
+    },
+    {
       path: '/',
       name: 'main-page',
       component: require('@/components/Main').default
