ba0ce5967f87a039956bdf380ba701cbc51fd91c
Meet Godhani <meetgodhani23@gmail.com>
2018-04-11 23:03:52 -0400

    Readme updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><img src="/ride-receipts.png" alt="Ride Receipts" width="150"></p>
+<p align="center"><img src="/ride-receipts.png" alt="Ride Receipts" width="350"></p>
 
 <p align="center">
 <a href="https://travis-ci.org/mrgodhani/uberrun">
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -12,8 +12,8 @@ Vue.use(BootstrapVue)
 const app = remote.app
 const documentDir = jetpack.cwd(app.getPath('documents'))
 
-if (!jetpack.exists(documentDir.path('Uber Run'))) {
-  jetpack.dir(documentDir.path('Uber Run'))
+if (!jetpack.exists(documentDir.path('Ride Receipts'))) {
+  jetpack.dir(documentDir.path('Ride Receipts'))
 }
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
